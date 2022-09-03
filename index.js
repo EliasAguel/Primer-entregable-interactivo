655f520a8e7d2b88404952446e904755fddd51bf
EliasAguel <aguelelias@gmail.com>
2022-09-03 12:32:34 -0300

    agregando html y un pequeño calculo en java

---
+++ index.js
@@ -0,0 +1,11 @@
+let precioPersona = (cantidadPersonas, precio) => cantidad * precio;
+let iva = (precioPersona) => precioPersona * 0.21;
+let precioFinal = (iva, otrosServicios) => iva = otrosServicios;
+
+if (cantidadPersonas = cantidadDisponible) {
+    alert (`El precio final es ${precioFinal}`);
+}
+else {
+    alert (`La cantidad de entradas solicitadas no estan disponibles`)
+}
+
